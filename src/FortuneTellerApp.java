c5ec70487e264ed21120cc9322aac3197ada496f
bryancharleslong <bryancharleslong@gmail.com>
2019-01-05 15:46:50 -0500

    base funtionality coded

--- src/FortuneTellerApp.java
+++ src/FortuneTellerApp.java
@@ -1,9 +1,105 @@
+import java.util.Scanner;
 
 public class FortuneTellerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to Fortune Teller!");
+		Scanner input = new Scanner(System.in);
 
+		System.out.println("What is your first name?");
+		String firstName = input.nextLine();
+		firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
+
+		System.out.println("What is your last name?");
+		String lastName = input.nextLine();
+		lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
+
+		System.out.println("What is your age?");
+		int age = input.nextInt();
+
+		System.out.println("What is your birth month? (1-12)");
+		int birthMonth = input.nextInt();
+
+		input.nextLine(); // becaue line return is in console buffer
+		System.out.println("What is your favorite color? (ROYGBIV)");
+		String favoriteColor = input.nextLine();
+
+		System.out.println("How many siblings do you have?");
+		int siblingNumber = input.nextInt();
+
+		System.out.println("Hello " + firstName + " " + lastName + "! Your age is " + age + ". Your birth month is "
+				+ birthMonth + ". Your favorite color is " + favoriteColor);
+		System.out.println("You have " + siblingNumber + " siblings");
+
+		input.close();
+
+		int retirementYears = 0;
+		if (age % 2 == 1) {
+			retirementYears = 14;
+		} else {
+			retirementYears = 12;
+		}
+
+		String vacationHomeLocation = null;
+		if (siblingNumber == 0) {
+			vacationHomeLocation = "Boca Raton, FL";
+		}
+		if (siblingNumber == 1) {
+			vacationHomeLocation = "Nassau, Bahamas";
+		}
+		if (siblingNumber == 2) {
+			vacationHomeLocation = "Ponta Negra, Brazil";
+		}
+		if (siblingNumber == 3) {
+			vacationHomeLocation = "Portland, Oregon";
+		}
+		if (siblingNumber > 3) {
+			vacationHomeLocation = "Baton Rouge, LA";
+		}
+		if (siblingNumber < 0) {
+			vacationHomeLocation = "Chernobyl, Ukraine";
+		}
+
+		String modeOfTransportaion = null;
+		if (favoriteColor.equals("red")) {
+			modeOfTransportaion = "Maserati";
+		}
+		if (favoriteColor.equals("orange")) {
+			modeOfTransportaion = "stallion";
+		}
+		if (favoriteColor.equals("yellow")) {
+			modeOfTransportaion = "chariot";
+		}
+		if (favoriteColor.equals("green")) {
+			modeOfTransportaion = "taxi";
+		}
+		if (favoriteColor.equals("blue")) {
+			modeOfTransportaion = "rickshaw";
+		}
+		if (favoriteColor.equals("indigo")) {
+			modeOfTransportaion = "motor scooter";
+		}
+		if (favoriteColor.equals("violet")) {
+			modeOfTransportaion = "flying saucer";
+		}
+
+		String bankBalance = null;
+		if (birthMonth < 5 & birthMonth > 0) {
+			bankBalance = "$256,000.76";
+		} else {
+			if (birthMonth < 9 & birthMonth > 4) {
+				bankBalance = "$3,687,105.42";
+			} else {
+				if (birthMonth < 13 & birthMonth > 8) {
+					bankBalance = "$86.23";
+				} else {
+					bankBalance = "0.00";
+				}
+			}
+		}
+		System.out.println(firstName + " " + lastName + " will retire in " + retirementYears + " years with "
+				+ bankBalance + " in the bank,");
+		System.out.println("a vacatin home in " + vacationHomeLocation + ", and travel by " + modeOfTransportaion);
 	}
 
 }
